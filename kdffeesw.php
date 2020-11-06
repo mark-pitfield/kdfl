e200bfd9e19560de62c7be112985475755aceecd
Mark Pitfield <mark.pitfield@cellxion.net>
2020-11-06 19:26:05 +0000

    updated rule changes

---
+++ kdffeesw.php
@@ -0,0 +1,476 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+
+
+<html>
+
+<meta name="format-detection">
+<head>
+<title>Kingston & District Football League.....</title>
+
+<link rel="stylesheet" type="text/css"href="kdfl.css" />
+
+
+<style type="text/css">
+</html>
+<?php
+$_SESSION['linkno'] = '16'
+?>
+<html>
+
+table.place
+{margin-left: 42px;
+font-family: Verdana, Geneva, Arial, Helvetica, sans-serif ;
+font-weight: normal;
+font-size: 12px;
+color: #ffffff;
+width: 500px;
+background-color: #606060;
+border: 0px;
+border-collapse: collapse;
+border-spacing: 0px;}
+
+table.place td
+{background-color: #C0C0C0;
+color: #000000;
+font-size: 20px;
+padding: 4px;
+text-align: center;
+border: 1px #fff solid;}
+
+table.place td.hed
+{background-color: #333333;
+color: #ffffff;
+padding: 4px;
+text-align: center;
+border-bottom: 2px #fff solid;
+font-size: 20px;
+font-weight: bold;}
+
+
+table.bt
+{margin-left: 42px;
+font-family: Verdana, Geneva, Arial, Helvetica, sans-serif ;
+font-weight: normal;
+color: #ffffff;
+width: 500px;
+background-color: #cococo;
+border: 0px;
+border-collapse: collapse;
+border-spacing: 0px;}
+
+table.bt td
+{background-color: #ee0000;
+color:#eeee00;
+font-size:36px;
+font-weight:700;
+padding: 4px;
+text-align: center;
+border: 1px #fff solid;}
+
+table.bt td.hed
+{background-color: #333333;
+color: #ffffff;
+padding: 4px;
+font-size: 12px;
+text-align: center;
+border-bottom: 2px #fff solid;
+font-size: 20px;
+font-weight: bold;
+}
+
+ td#box a:link {color:#eeee00;background:#ee0000;}
+ td#box a:visited {color:#eeee00;background:#ee0000;}
+ td#box a:hover {color:#eeee00;background:#ee0000;}
+ td#box a:active {color:#eeee00;background:#ee0000;}
+
+ .feetable {
+   font-family: arial;
+   padding-bottom: 30px;
+   width: 560px;
+ }
+ .feehead {
+   font-weight:bold;
+   color:white;
+   font-size:1.5rem;
+ }
+ .feeheadfee {
+   text-align: right;
+ }
+ .feedata {
+   color:yellow;
+   text-align:center;
+   vertical-align: top;
+ }
+ .feedatafee {
+   text-align: right;
+ }
+
+</style>
+
+</head>
+
+
+<body>
+
+<div id="all">
+
+	<div id="header"><h1 class="top">Kingston & District Football League</h1></div>
+
+	<div id="one">&nbsp</div>
+
+	<div id="strip"><IMG src="DSCF0200.jpg"><IMG src="DSCF0196cx.jpg"><IMG src="DSCF0194c.jpg"><IMG src="DSCF0105.jpg"><IMG src="kdflbadgex.jpg"><IMG src="DSCF0211x.jpg"><IMG src="ll1.jpg"><IMG src="wolves2.jpg"><IMG src="IMG_0767.jpg">
+</div>
+	<!-- Below is the red bar and the black bars above and below it -->
+	<div id="one">&nbsp</div>
+	<div id="bar">&nbsp</div>
+	<div id="two">&nbsp</div>
+
+	<div id="inside"style="height: 1800px;">
+        <!-- 1800px is distance between red and green bars -->
+	<div id="content2"><h2 class="deh"></h2>
+
+    <!-- <p class="subheadw">Fees and fines - All amounts are GBP</p> -->
+    <div align="left" style="width:490px;";>
+
+
+    <table class="feetable">
+      <tr class="feehead">
+        <th width="5%" >Rule</th>
+        <th width="75%" >Fees Tariff - Description</th>
+        <th width="20%" class="feeheadfee" >Max Fee</th>
+      </tr>
+      <tr class="feedata">
+        <td>4A</td>
+        <td>Entry Fee</td>
+        <td class="feedatafee">£0</td>
+      </tr>
+      <tr class="feedata">
+        <td>4B</td>
+        <td>Annual Subscription per team</td>
+        <td class="feedatafee">£60</td>
+      </tr>
+      <tr class="feedata">
+        <td>4C</td>
+        <td>Deposit per team</td>
+        <td class="feedatafee">£100</td>
+      </tr>
+      <tr class="feedata">
+        <td>4F</td>
+        <td>Purchase of League Handbooks (per copy)</td>
+        <td class="feedatafee">£5</td>
+      </tr>
+      <tr class="feedata">
+        <td>4G</td>
+        <td>Contribution to League Presentation evening (per club)</td>
+        <td class="feedatafee">£60</td>
+      </tr>
+      <tr class="feedata">
+        <td></td>
+        <td>Additional Teams (each)</td>
+        <td class="feedatafee">£30</td>
+      </tr>
+      <tr class="feedata">
+        <td>7C,F,H</td>
+        <td>Appeals and Protest Fees</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+      <tr class="feedata">
+        <td>18D</td>
+        <td>Player registration form (per 10 copies)</td>
+        <td class="feedatafee">£2</td>
+      </tr>
+      <tr class="feedata">
+        <td>18H</td>
+        <td>Player transfer form</td>
+        <td class="feedatafee">£2</td>
+      </tr>
+      <tr class="feedata">
+        <td>23E</td>
+        <td>Referee Fees per game</td>
+        <td class="feedatafee">£40</td>
+      </tr>
+      <tr class="feedata">
+        <td></td>
+        <td>Referee Fees for Two one houe games</td>
+        <td class="feedatafee">£50</td>
+      </tr>
+      <tr class="feedata">
+        <td></td>
+        <td>Assistant Referee Fee</td>
+        <td class="feedatafee">£30</td>
+      </tr>
+    </table>
+
+
+    <table class="feetable">
+      <tr class="feehead">
+        <th width="5%" >Rule</th>
+        <th width="75%" >Fines Tariff - Description</th>
+        <th width="20%" class="feeheadfee" >Max Fee</th>
+      </tr>
+      <tr class="feedata">
+        <td>2G</td>
+        <td>Failure to be correctly affiliated</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+      <tr class="feedata">
+        <td>2I</td>
+        <td>Failure to comply with FA initictives</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+      <tr class="feedata">
+        <td>2K</td>
+        <td>Unauthorised entry of teams into competitions</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+      <tr class="feedata">
+        <td>3</td>
+        <td>Failure to obtain consent for change of club name</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+      <tr class="feedata">
+        <td>4B</td>
+        <td>All league fees must be paid on or before the August meeting</td>
+        <td class="feedatafee">£50</td>
+      </tr>
+      <tr class="feedata">
+        <td>4C</td>
+        <td>Failure to pay deposit for membership</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+      <tr class="feedata">
+        <td>4E</td>
+        <td>Failure to provide affiliation number and club details by required date</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+      <tr class="feedata">
+        <td>4H</td>
+        <td>Failure to collect award at presentation night</td>
+        <td class="feedatafee">£50</td>
+      </tr>
+      <tr class="feedata">
+        <td>5F</td>
+        <td>Failure to be represented at General Monthly meetings</td>
+        <td class="feedatafee">£30</td>
+      </tr>
+      <tr class="feedata">
+        <td>6H</td>
+        <td>Failure to comply with an instruction of the managment committee or failing to attend to the business and/or correspndence of the competition</td>
+        <td class="feedatafee">£50</td>
+      </tr>
+      <tr class="feedata">
+        <td>6I</td>
+        <td>Failure to pay a fine within 14 days</td>
+        <td class="feedatafee">£25</td>
+      </tr>
+      <tr class="feedata">
+        <td>8H</td>
+        <td>Failure to be represented at the AGM</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+      <tr class="feedata">
+        <td>9</td>
+        <td>Failure to be represented at the SEM</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+      <tr class="feedata">
+        <td>10</td>
+        <td>Failure to submit the required written agreement or to notify changes to signatories</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+      <tr class="feedata">
+        <td>11A</td>
+        <td>Failure to provide notice of withdrawal before deadline</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+      <tr class="feedata">
+        <td>11B</td>
+        <td>Failure to commence/complete fixtures</td>
+        <td class="feedatafee">£50</td>
+      </tr>
+      <tr class="feedata">
+        <td>13A</td>
+        <td>Failure to provide the written agreement regarding the trophy</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+      <tr class="feedata">
+        <td>16A,B</td>
+        <td>Failure to have the required insurance</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+      <tr class="feedata">
+        <td>18A</td>
+        <td>Failure to correctly register a player</td>
+        <td class="feedatafee">£20</td>
+      </tr>
+      <tr class="feedata">
+        <td>18B</td>
+        <td>Failure to have the required number of registered players prior to the season commencing</td>
+        <td class="feedatafee">£20</td>
+      </tr>
+      <tr class="feedata">
+        <td>18F</td>
+        <td>Registering or playing for multiple clubs or inaccurate completion of a registration form</td>
+        <td class="feedatafee">£40</td>
+      </tr>
+      <tr class="feedata">
+        <td>18G</td>
+        <td>Registration irregularities</td>
+        <td class="feedatafee">£40</td>
+      </tr>
+      <tr class="feedata">
+        <td>18M</td>
+        <td>Fielding more than the permitted number of players who have participated in senior competition matches</td>
+        <td class="feedatafee">£40</td>
+      </tr>
+      <tr class="feedata">
+        <td>18N</td>
+        <td>Playing an ineligible player</td>
+        <td class="feedatafee">£40</td>
+      </tr>
+      <tr class="feedata">
+        <td>18O</td>
+        <td>Failure to give priority to school activities</td>
+        <td class="feedatafee">£5</td>
+      </tr>
+      <tr class="feedata">
+        <td>19</td>
+        <td>Delaying kick off time due to nochange of colours or no numbered shirts</td>
+        <td class="feedatafee">£20</td>
+      </tr>
+      <tr class="feedata">
+        <td>20A</td>
+        <td>Failure to start a fixture at the right time</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+      <tr class="feedata">
+        <td>20B</td>
+        <td>Failure to play a match on the date fixed</td>
+        <td class="feedatafee">£40</td>
+      </tr>
+      <tr class="feedata">
+        <td>20C</td>
+        <td>Failure to provide fixture details with 5 days clear notice</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+      <tr class="feedata">
+        <td>20D</td>
+        <td>Playing a match with less than 8 players</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+      <tr class="feedata">
+        <td>20E</td>
+        <td>Failure to play a fixture</td>
+        <td class="feedatafee">£40</td>
+      </tr>
+      <tr class="feedata">
+        <td>20H</td>
+        <td>Failure to identify a captain</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+      <tr class="feedata">
+        <td>20J</td>
+        <td>Failure to provide first aid kit, referee's whistle and linesman's flags</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+      <tr class="feedata">
+        <td>20K</td>
+        <td>Failure to provideplayers signatures</td>
+        <td class="feedatafee">£15</td>
+      </tr>
+      <tr class="feedata">
+        <td>21A</td>
+        <td>Late receipt of result documentation</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+      <tr class="feedata">
+        <td>21B</td>
+        <td>Failure to notify match result on time</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+      <tr class="feedata">
+        <td>21C</td>
+        <td>Match result notification not correctly completed</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+      <tr class="feedata">
+        <td>23C</td>
+        <td>Failure to provide a club assitant referee</td>
+        <td class="feedatafee">£5</td>
+      </tr>
+      <tr class="feedata">
+        <td>23E</td>
+        <td>Failure to pay match officals fees</td>
+        <td class="feedatafee">£40</td>
+      </tr>
+      <tr class="feedata">
+        <td>23F</td>
+        <td>Failure to pay match officals when match not played</td>
+        <td class="feedatafee">£40</td>
+      </tr>
+      <tr class="feedata">
+        <td>23H</td>
+        <td>Failure to provide referee's mark</td>
+        <td class="feedatafee">£10</td>
+      </tr>
+    </table>
+
+&nbsp
+
+</div>
+
+
+</div>
+
+	<div id="menu" >
+	 <P class="nine">OTHER PAGES</P>
+
+
+<?php
+include 'intnllinks.php'
+?>
+
+      <br/>
+
+      <hr noshade align=center size=6 color="#ff6600" width=218>
+
+	 <P class="nine">OTHER WEBSITES</P>
+
+
+<?php
+include 'extnllinks.php'
+?>
+
+</div>
+	<!-- Below is the green bar and the black above and below it -->
+	<div id="one1">&nbsp</div>
+	<div id="bar1">&nbsp</div>
+	<div id="two1">&nbsp</div>
+
+</div>
+
+</body>
+
+</html>
+<script>
+var repeat=1
+var title=document.title
+var leng=title.length
+var start=1
+function titlemove() {
+  titl=title.substring(start, leng) + title.substring(0, start)
+  document.title=titl
+  start++
+  if (start==leng+1) {
+    start=0
+    if (repeat==0)
+    return
+  }
+  setTimeout("titlemove()",200)
+}
+if (document.title)
+titlemove()
+</script>
