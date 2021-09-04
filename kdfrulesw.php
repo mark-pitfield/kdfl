60dbc71f28ebe324f1e739111440d23fc2d4047d
Mark Pitfield <mpitfield@gmail.com>
2021-09-04 13:49:40 +0100

    Open in new window

--- kdfrulesw.php
+++ kdfrulesw.php
@@ -122,7 +122,7 @@ font-weight: bold;
 
 
 <div class="cssbutton">
-<a href="kdflrules-2019-2020.pdf"><img src="down2.jpg" alt="LEAGUE RULES" /><span>LEAGUE RULES</span></a>
+<a href="kdflrules-2019-2020.pdf" target="_blank"><img src="down2.jpg" alt="LEAGUE RULES" /><span>LEAGUE RULES</span></a>
 </div>
 
 
